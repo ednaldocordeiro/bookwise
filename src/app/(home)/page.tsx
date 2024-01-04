d23ca1bccfaec577b305609cf2d707c6fddc6e70
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2024-01-04 10:47:44 -0300

    home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,19 @@ import { getServerSession } from 'next-auth'
 
 export default async function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Home</h1>
+    <div className="flex flex-1 flex-col p-10">
+      <div className="flex items-center gap-3">
+        <LineChart className="text-3xl text-bw-green-100" />
+        <h1 className="text-2xl font-bold leading-tight">Início</h1>
+      </div>
+      <div className="mt-10 grid grid-cols-3 gap-16">
+        <div className="col-span-2 h-full w-full">
+          <h1>asaaaa</h1>
+        </div>
+        <div className="col-span-1 h-full w-full">
+          <h1>asaaaa</h1>
+        </div>
+      </div>
     </div>
   )
 }
