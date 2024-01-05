4a98c4b3ae6c549ccd6abf08e759139e8533bad0
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2024-01-05 16:01:41 -0300

    sidebar style adjustment

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,7 +8,7 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options'
 export async function SideBar() {
   const session = await getServerSession(authOptions)
   return (
-    <aside className="col-span-1 flex h-full w-[320px] flex-col items-center gap-12 rounded-lg bg-sidebar bg-cover bg-no-repeat p-10">
+    <aside className="m-w-[320px] flex h-full flex-col items-center gap-12 rounded-lg bg-sidebar bg-cover bg-no-repeat p-10">
       <Image src="/logo.svg" alt="" width={150} height={40} />
       <Routes session={session} />
       {session && session.user && (
