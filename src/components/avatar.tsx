4d0a539947a6e3e145fdb8a19149e6532bcb48f8
Ednaldo Cordeiro <ednaldocordeiro2017@gmail.com>
2024-01-23 17:54:44 -0300

    avatar cursor when can redirect to profile

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -6,20 +6,17 @@ import { useRouter } from 'next/navigation'
 
 import { merge } from '@/utils/tw-werge'
 
-const avatarVariants = cva(
-  'h-20 w-20 rounded-full object-cover cursor-pointer',
-  {
-    variants: {
-      size: {
-        md: 'h-10 w-10',
-        xl: 'h-20 w-20',
-      },
-    },
-    defaultVariants: {
-      size: 'md',
+const avatarVariants = cva('h-20 w-20 rounded-full object-cover', {
+  variants: {
+    size: {
+      md: 'h-10 w-10',
+      xl: 'h-20 w-20',
     },
   },
-)
+  defaultVariants: {
+    size: 'md',
+  },
+})
 
 interface AvatarProps
   extends React.ButtonHTMLAttributes<HTMLImageElement>,
@@ -45,7 +42,10 @@ export function Avatar({
       alt=""
       width={80}
       height={80}
-      className={merge(avatarVariants({ size, className }))}
+      className={merge(
+        avatarVariants({ size, className }),
+        redirectToProfile ? 'cursor-pointer' : 'cursor-default',
+      )}
       onClick={() => redirectToProfile && router.push(`/profile/${userId}`)}
       placeholder={'blur'}
       blurDataURL="/person.png"
